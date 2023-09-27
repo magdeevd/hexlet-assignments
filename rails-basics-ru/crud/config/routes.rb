ccb02ffe44e0576773e1619ea6e886230c2bece5
@hexlet/cli <support@hexlet.io>
2023-09-27 13:32:01 +0400

    submit rails-basics-ru/crud

---
+++ rails-basics-ru/crud/config/routes.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  root 'home#index'
+  # BEGIN
+  resources :tasks, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  # END
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
---
+++ rails-basics-ru/crud/test/controllers/tasks_controller_test.rb
@@ -0,0 +1,58 @@
+require "test_helper"
+
+class TasksControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @attrs = {
+      name: Faker::Artist.name,
+      description: Faker::Movies::HarryPotter.quote,
+      status: Faker::Movies::HarryPotter.spell,
+      creator: Faker::Movies::HarryPotter.character,
+      performer: Faker::Movies::HarryPotter.character,
+      completed: Faker::Boolean.boolean
+    }
+  end
+
+  test '#index' do
+    get tasks_url
+    assert_response :success
+  end
+
+  test '#show' do
+    task = Task.create(@attrs)
+    get task_url(task)
+    assert_response :success
+  end
+
+  test '#new' do
+    get new_task_url
+    assert_response :success
+  end
+
+  test '#create' do
+    post tasks_url, params: { task: @attrs }
+    assert { Task.find_by(@attrs) }
+    assert_response :redirect
+  end
+
+  test '#edit' do
+    task = Task.create(@attrs)
+    get edit_task_url task
+    assert_response :success
+  end
+
+  test '#update' do
+    task = Task.create(@attrs)
+    patch task_url(task), params: { task: @attrs }
+    assert { Task.find_by(@attrs) }
+    assert_response :redirect
+  end
+
+  test '#destroy' do
+    task = Task.create(@attrs)
+    delete task_url(task)
+    assert { !Task.find_by(@attrs) }
+    assert_response :redirect
+  end
+
+  private
+end
